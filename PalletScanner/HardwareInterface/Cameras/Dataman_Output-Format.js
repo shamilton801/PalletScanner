dad14b08fce980caf46059133844548e101f45a8
Jordan Lamkin <jlamkin@logisolve.com>
2025-07-16 13:09:57 -0500

    Updated Cognex script to v2.0

--- PalletScanner/HardwareInterface/Cameras/Dataman_Output-Format.js
+++ PalletScanner/HardwareInterface/Cameras/Dataman_Output-Format.js
@@ -1,35 +1,42 @@
-var name = dmccGet("DEVICE.NAME").response;
+function getHeaderInfo(readerProperties) {
+	return [
+		readerProperties.name,
+		readerProperties.trigger.creationTime
+	];
+}
+function getInfoPerResult(decodeResult) {
+	var corners = decodeResult.symbology.corners;
+	var cornerString = corners.map(function(corner) {
+		return corner.x + "," + corner.y;
+	}).join("\n");
+	return [
+		decodeResult.symbology.center.x,
+		decodeResult.symbology.center.y,
+		decodeResult.symbology.angle,
+		decodeResult.symbology.name,
+		decodeResult.symbology.moduleSize,
+		encode_base64(cornerString),
+		encode_base64(decodeResult.content)
+	];
+}
+var VersionNumber = "2.0"; // NOTE: UPDATE THIS WHENEVER THE OUTPUT FORMAT CHANGES!!!
 
+function FormatOutput(headerInfo, bodyInfo) {
+	var lines = [
+		["GateKeeper Reader Format " + VersionNumber],
+		headerInfo
+	].concat(bodyInfo);
+	return lines.map(function(line) {
+		return line.join(",");
+	}).join("\n");
+}
 function onResult(decodeResults, readerProperties, output) {
-	var s = name;
-	s += ",";
-	s += readerProperties.trigger.creationTime;	// Does not change
-
-	s += "\n";
-	if (decodeResults[0].decoded) {
-
-		for (var i = 0; i < decodeResults.length; i++) {
-			if (!decodeResults[i].decoded) continue;
-			if (decodeResults[i].source != name) continue;
-			if (decodeResults[i].decoded) {
-				s += decodeResults[i].symbology.center.x;
-				s += ",";
-				s += decodeResults[i].symbology.center.y;
-				s += ",";
-				s += decodeResults[i].symbology.angle;
-				s += ",";
-				s += decodeResults[i].symbology.name;
-				s += ",";
-				s += decodeResults[i].symbology.moduleSize;
-				s += ",";
-				s += decodeResults[i].content;
-				if (i < decodeResults.length - 1) {
-					s += "\n";
-				}
-			}
-		}
-		s += "\r";
-		output.content = s;
+	var headerInfo = getHeaderInfo(readerProperties);
+	var bodyInfo = [];
+	for(var i = 0; i < decodeResults.length; i++) {
+		var decodeResult = decodeResults[i];
+		if (!decodeResult.decoded) continue;
+		bodyInfo.push(getInfoPerResult(decodeResult));
 	}
-
+	output.content = FormatOutput(headerInfo, bodyInfo) + "\r";
 }
