7b243f636d9a5ef9a17608454f4505e5c830160d
Jordan Lamkin <jlamkin@logisolve.com>
2025-07-16 20:17:01 -0500

    Added camera parameters

---
+++ Source/PalletScanner/Data/Pose.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+
+namespace PalletScanner.Data
+{
+    /// <summary>
+    /// <para>
+    /// Represents a rigid transformation from one coordinate system to another. <b>NOTE: By default, unless otherwise
+    /// stated, the <c>Pose</c> of an object transforms its local space to the ambient world space. </b>
+    /// </para>
+    /// <para>
+    /// Given two <c>Pose</c>s <c>p1</c> and <c>p2</c> which transform from space A to B and from B to C respectively,
+    /// the pose <c>p2 * p1</c> transforms from A to C, and <c>p1 * v</c> transforms a vector <c>v</c> from space A to
+    /// B. A <c>Pose</c> has a rotation and a translation. The rotation is always applied first, followed by the
+    /// translation.
+    /// </para>
+    /// </summary>
+    /// <param name="rotation">The rotation of this pose.</param>
+    /// <param name="translation">The translation of this pose.</param>
+    public struct Pose(Quaternion rotation, Vector3 translation)
+    {
+        public Vector3 Translation { readonly get; set; } = translation;
+        public Quaternion Rotation { readonly get; set; } = rotation;
+
+        public static Pose Identity => new(Quaternion.Identity, Vector3.Zero);
+
+        public Pose(Vector3 translation) : this(Quaternion.Identity, translation) { }
+        public Pose(Quaternion rotation) : this(rotation, Vector3.Zero) { }
+
+        public static Vector3 operator *(Pose p, Vector3 v) => Vector3.Transform(v, p.Rotation) + p.Translation;
+        public static Pose operator *(Pose a, Pose b) => new(a.Rotation * b.Rotation, a * b.Translation);
+        public readonly Pose Inverse
+        {
+            get
+            {
+                var rotInv = Quaternion.Conjugate(Rotation);
+                return new(rotInv, -Vector3.Transform(Translation, rotInv));
+            }
+        }
+    }
+}
--- Source/PalletScanner/Hardware/Cameras/ICamera.cs
+++ Source/PalletScanner/Hardware/Cameras/ICamera.cs
@@ -1,6 +1,7 @@
 ﻿using PalletScanner.Data;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -8,10 +9,64 @@ using System.Threading.Tasks;
 
 namespace PalletScanner.Hardware.Cameras
 {
+    /// <summary>
+    /// Intrinsic camera parameters
+    /// </summary>
+    public struct CameraParameters
+    {
+        private PointF? PrincipalPointOverride;
+
+        /// <summary>
+        /// The size of each output frame.
+        /// </summary>
+        public Size CameraSize { readonly get; set; }
+        /// <summary>
+        /// Number of pixels away from the PrincipalPoint at 45 degrees from the optical axis.
+        /// </summary>
+        public float FocalLengthPixels { readonly get; set; }
+        /// <summary>
+        /// The pixel location of the optical axis.
+        /// </summary>
+        public PointF PrincipalPoint
+        {
+            readonly get => PrincipalPointOverride ??
+                new(CameraSize.Width / 2.0f, CameraSize.Height / 2.0f);
+            set => PrincipalPointOverride = value;
+        }
+        /// <summary>
+        /// Internally reset the PrincipalPoint, tying it to the center of CameraSize.
+        /// </summary>
+        public void UseDefaultPrincipalPoint() => PrincipalPointOverride = null;
+    }
+
     public interface ICamera
     {
         string Name { get; }
         IAsyncEnumerable<BarcodeRead> ReadBarcodes();
+
+        /// <summary>
+        /// Local axes:
+        /// <list type="table">
+        /// <listheader>
+        ///     <term>Axis</term>
+        ///     <description>Local Direction</description>
+        /// </listheader>
+        /// <item>
+        ///     <term>X</term>
+        ///     <description>Right</description>
+        /// </item>
+        /// <item>
+        ///     <term>Y</term>
+        ///     <description>Down</description>
+        /// </item>
+        /// <item>
+        ///     <term>Z</term>
+        ///     <description>Backward</description>
+        /// </item>
+        /// </list>
+        /// </summary>
+        Pose? CameraPose => null;
+        CameraParameters? CameraParams => null;
     }
 
     public static class CameraEx
