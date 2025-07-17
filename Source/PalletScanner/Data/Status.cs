f3ecd8c27968b5cf3033b519b072b26f38605a20
Seth Hamilton <shamilton@the-embedded-lab.com>
2025-07-17 16:38:02 -0500

    Add child status to the Status IF

--- Source/PalletScanner/Data/Status.cs
+++ Source/PalletScanner/Data/Status.cs
@@ -9,17 +9,7 @@
     {
         public StatusType Type => type;
         public string Message => message;
-        public virtual IEnumerable<BarcodeRead> AssociatedBarcodeReads => [];
-    }
-
-    public class BarcodeReadStatus(StatusType type, BarcodeRead barcode, string message = "") : Status(type, message)
-    {
-        public override IEnumerable<BarcodeRead> AssociatedBarcodeReads => [ barcode ];
-    }
-
-    public class BarcodeReadListStatus(StatusType type, string message = "") : Status(type, message)
-    {
-        public override IEnumerable<BarcodeRead> AssociatedBarcodeReads => BarcodeReads;
-        public readonly List<BarcodeRead> BarcodeReads = [];
+        public IEnumerable<BarcodeRead> AssociatedBarcodeReads = [];
+        public IEnumerable<Status> ChildStatus = [];
     }
 }
